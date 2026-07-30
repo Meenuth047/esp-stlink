1e34f71dcce11d92bcbc7e71dffd75977950b86b
Meenu Thakur <meenuthakur088@gmail.com>
2026-07-30 16:59:13 +0530

    Update(readout_protection.py):To disable the HC12

--- python/readout_protection.py
+++ python/readout_protection.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # Turns readout protection on / off
 
+import time
 import espstlink
 from espstlink.flash import Options
 
@@ -24,13 +25,23 @@ if __name__ == '__main__':
     import sys
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--device", default='/dev/ttyUSB0',
+    parser.add_argument("-d", "--device", default='/dev/ttyUSB1',
                         help="The serial device the HC is connected to")
-    parser.add_argument("enable_rop", type=bool,
-                        help="Whether to enable ROP or not", nargs='?')
+    def str2bool(v):
+        if isinstance(v, bool):
+            return v
+        if v.lower() in ('yes', 'true', 't', 'y', '1', 'on'):
+            return True
+        elif v.lower() in ('no', 'false', 'f', 'n', '0', 'off'):
+            return False
+        else:
+            raise argparse.ArgumentTypeError('Boolean value expected.')
+
+    parser.add_argument("enable_rop", type=str2bool,
+                        help="Whether to enable ROP or not (true/false/on/off)", nargs='?')
     args = parser.parse_args()
 
-    r = ReadoutProtection()
-    print('ROP', r.options['ROP'].status())
-    if args.rop is not None:
+    r = ReadoutProtection(espstlink.STLink(args.device.encode()))
+    print('ROP status before:', r.options['ROP'].status())
+    if args.enable_rop is not None:
       r.set(args.enable_rop)
